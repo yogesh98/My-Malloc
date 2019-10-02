5649525f18eb8a5ee12ecb18b2e4eb896fb9950e
Aditya J. Patel <ajp365@cray1.cs.rutgers.edu>
2019-10-02 15:52:28 -0400

    made a main file with header and some minor changes to the header file

--- mymalloc.h
+++ mymalloc.h
@@ -4,6 +4,7 @@
 #define malloc(x) myMalloc(x)
 #define free(x) myFree(x)
 #define true 1
-
+//need a library file that uses the header file for functions.
 
 static char myblock[4096];
+int* integers = (integers*) myblock; // used to store integers by bytes along with char
